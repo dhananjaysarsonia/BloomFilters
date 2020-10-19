cac18c55ccb6e100ff08d43a4949ac63ec0d6154
Dhananjay <dhananjaysarsonia@outlook.com>
2020-10-19 11:00:35 -0400

    Initial commit

---
+++ src/com/company/BloomFilter.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BloomFilter {
+
+    private int numElements, filterSize, numHashes;
+    public BloomFilter(int numElements, int filterSize, int numHashes){
+
+    }
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        //BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+}
